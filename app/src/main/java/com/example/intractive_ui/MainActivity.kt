c1eb5a24f09d5267ab9c88e3b257d36789d4988c
Azhar Qureshi <azhar91973@gmail.com>
2024-04-14 09:39:10 +0530

    Enabled View Binding

--- app/src/main/java/com/example/intractive_ui/MainActivity.kt
+++ app/src/main/java/com/example/intractive_ui/MainActivity.kt
@@ -5,34 +5,25 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.example.intractive_ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        // Writing the Functionality of the buttons
-        val DarkButton = findViewById<Button>(R.id.btnDarkMode)
-        val ReadModeButton = findViewById<Button>(R.id.btnReadMode)
-        
-        // Taking an instance of layout bcz we have to enable dark and read mode within a layout
-        val layout = findViewById<LinearLayout>(R.id.newlayout)
-
-        // Taking a TextField to change the color of text while doing dark and read mode
-        val textfield1 = findViewById<TextView>(R.id.textView)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) 
         // For Dark button to enable dark mode
-        DarkButton.setOnClickListener(){
+        binding.btnDarkMode.setOnClickListener(){
             // when Dark button get clicked then  background will be change to dark mode
-            layout.setBackgroundResource(R.color.black)
+           binding.newlayout.setBackgroundResource(R.color.black)
             // and the text change to white in color
-            textfield1.setTextColor(getColor(R.color.white))
+            binding.textView.setTextColor(getColor(R.color.white))
         }
         // For Read button to enable Read mode
-        ReadModeButton.setOnClickListener(){
-            layout.setBackgroundResource(R.color.warm)
-            textfield1.setTextColor(getColor(R.color.black))
+        binding.btnReadMode.setOnClickListener(){
+            binding.newlayout.setBackgroundResource(R.color.warm)
+            binding.textView.setTextColor(getColor(R.color.black))
         }
 
     }
